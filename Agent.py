aff5b18cef06147e0991cf0ca73b5df5c71755f6
DESKTOP-AI6RF7O\Sherman Luo <shermanluo@berkeley.edu>
2018-03-05 00:33:07 -0800

    heat agents / helper agents

--- Agent.py
+++ Agent.py
@@ -124,7 +124,11 @@ class HelperMinimaxAgentDiscount(Agent):
 			nxt = gameState.getSuccessor(player, action)
 			value = self.value(nxt, 2, depth)
 			if depth == self.depth - 1:
-				print("MOVE: ", action, " LEADS TO:")
+				print("MOVE: ", action, " PROBABLY LEADS TO:")
+				if value[0] < 0:
+					print("DANGER OF LOSING")
+				if value[0] > 0:
+					print("GUARANTEED WIN")
 				value[2].printBoard()
 				print()
 
@@ -138,7 +142,11 @@ class HelperMinimaxAgentDiscount(Agent):
 			nxt = gameState.getSuccessor(player, action) 
 			value = self.value(nxt, 1, depth)
 			if depth == self.depth - 1:
-				print("MOVE: ", action, " LEADS TO:")
+				print("MOVE: ", action, " PROBABLY LEADS TO:")
+				if value[0] > 0:
+					print("DANGER OF LOSING")
+				if value[0] < 0:
+					print("GUARANTEED WIN")
 				value[2].printBoard()
 				print()
 
@@ -158,9 +166,142 @@ class HelperMinimaxAgentDiscount(Agent):
 
 
 
+class HeatAgentDiscount(Agent):
+	def __init__(self, depth = 10, alpha = 1, discount = 1):
+		self.depth = depth
+		self.alpha = alpha
+		self.discount = discount
 
+	def value(self, gameState, player, depth):
+		#Tie Game
+		if gameState.isWin() and gameState.isLose():
+			return (0, None)
+		if depth == -1:
+			return (self.evaluationFunction(gameState), None)
+		if gameState.isWin():
+			return (5, None)
+		if gameState.isLose():
+			return (-5, None)
+		if player == 1:
+			return self.maxValue(gameState, 1, depth - 1)
+		else:
+			return self.minValue(gameState, 2, depth - 1)
 
+	def maxValue(self, gameState, player, depth):
+		totalValue = 0
+		d = {}
+		for action in gameState.getLegalActions(player):
+			nxt = gameState.getSuccessor(player, action)
+			val = self.value(nxt, 2, depth)[0] * self.discount
+			totalValue += pow(2, self.alpha * val)
+			d[action] = val
+		r = random.uniform(0, 1)
+		running = 0
+		for key in d:
+			running += pow(2, self.alpha * d[key]) / totalValue
+			if r <= running:
+				return (d[key], key)
 
+	def minValue(self, gameState, player, depth):
+		totalValue = 0
+		d = {}
+		for action in gameState.getLegalActions(player):
+			nxt = gameState.getSuccessor(player, action) 
+			val = self.value(nxt, 1, depth)[0] * self.discount
+			totalValue += pow(2, self.alpha * val * -1)
+			d[action] = val
+		r = random.uniform(0, 1)
+		running = 0
+		for key in d:
+			running += pow(2, self.alpha * d[key] * -1) / totalValue
+			if r <= running:
+				return (d[key], key)
+
+	def getAction(self, gameState, player):
+		t = self.value(gameState, player, self.depth)
+		return t[1]
+		
+	def evaluationFunction(self, gameState):
+		return 0
+
+class HeatAgentDiscountHelper(Agent):
+	def __init__(self, depth = 10, alpha = 1, discount = 1):
+		self.depth = depth
+		self.alpha = alpha
+		self.discount = discount
+
+	def value(self, gameState, player, depth):
+		#Tie Game
+		if gameState.isWin() and gameState.isLose():
+			return (0, None, gameState)
+		if depth == -1:
+			return (self.evaluationFunction(gameState), None, gameState)
+		if gameState.isWin():
+			return (5, None, gameState)
+		if gameState.isLose():
+			return (-5, None, gameState)
+		if player == 1:
+			return self.maxValue(gameState, 1, depth - 1)
+		else:
+			return self.minValue(gameState, 2, depth - 1)
+
+	def maxValue(self, gameState, player, depth):
+		totalValue = 0
+		d = {}
+		g = {}
+		for action in gameState.getLegalActions(player):
+			nxt = gameState.getSuccessor(player, action)
+			value = self.value(nxt, 2, depth)
+			val = value[0] * self.discount
+			if depth == self.depth - 1:
+				print("MOVE: ", action, " PROBABLY LEADS TO:")
+				value[2].printBoard()
+				print()
+
+			totalValue += pow(2, self.alpha * val)
+			d[action] = val
+			g[action] = value[2]
+		if depth == self.depth - 1:
+			x = max(d, key = lambda x: d[x])
+			return (d[x], x, value[2])
+		r = random.uniform(0, 1)
+		running = 0
+		for key in d:
+			running += pow(2, self.alpha * d[key]) / totalValue
+			if r <= running:
+				return (d[key], key, g[key])
+
+	def minValue(self, gameState, player, depth):
+		totalValue = 0
+		d = {}
+		g = {}
+		for action in gameState.getLegalActions(player):
+			nxt = gameState.getSuccessor(player, action) 
+			value = self.value(nxt, 2, depth)
+			val = value[0] * self.discount
+			if depth == self.depth - 1:
+				print("MOVE: ", action, " PROBABLY LEADS TO:")
+				value[2].printBoard()
+				print()
+			totalValue += pow(2, self.alpha * val * -1)
+			d[action] = val
+			g[action] = value[2]
+		if depth == self.depth - 1:
+			x = min(d, key = lambda x: d[x])
+			return (d[x], x, value[2])
+		r = random.uniform(0, 1)
+		running = 0
+		for key in d:
+			running += pow(2, self.alpha * d[key] * -1) / totalValue
+			if r <= running:
+				return (d[key], key, g[key])
+
+	def getAction(self, gameState, player):
+		t = self.value(gameState, player, self.depth)
+		return t[1]
+		
+	def evaluationFunction(self, gameState):
+		return 0
 
 class RandomAgent(Agent):
 	def getAction(self, gameState, player):
